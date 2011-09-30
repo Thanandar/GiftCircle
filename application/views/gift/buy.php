8d920cccae6d74b22e6506548057cd8956be68c4
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-09-30 15:07:08 +0100

    Gift details when you buy

--- application/views/gift/buy.php
+++ application/views/gift/buy.php
@@ -2,15 +2,35 @@
 <div class="span12">
 
 	<h2>Shop for '<?php echo HTML::chars($gift->name); ?>' now</h2>
+	
+	<h3>
+		For <?php echo HTML::chars($gift->list->owner->firstname . ' ' . $gift->list->owner->surname); ?>'s list "<?php echo HTML::chars($gift->list->name); ?>"
+	</h3>
+	
+	<dl>
+		<dt>Approximate price</dt>
+			<dd>&pound;<?php echo HTML::chars($gift->price); ?></dd>
+		<dt>Category</dt>
+			<dd><?php echo HTML::chars($gift->category->name); ?></dd>
+		<dt>Description</dt>
+			<dd><?php echo HTML::chars($gift->details); ?></dd>
+	</dl>
+
 	<p>Click a retailer logo to browse. Mark the gift as bought after you've bought it.</p>
 
 	<div class="well">
+
+		<?php if ($gift->url) { ?>
+		<input type="button" class="btn primary" value="Buy from preset URL" onclick="window.open('<?php echo HTML::chars($gift->url); ?>')" />
+		
+		<?php } ?>
+
 		<input type="button" class="btn" value="Mark as bought" onclick="location.href='/gift/bought/<?php echo $gift->id; ?>'" />
 	</div>
 
 	<hr />
 
-	<h3>Category &gt; <?php echo $gift->category->name; ?></h3>
+	<h3>Category &gt; <?php echo HTML::chars($gift->category->name); ?></h3>
 
 	<ul class="media-grid">
 		<li>
