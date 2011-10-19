3f395183e0f04d3ab3a3940a9630dde019988f70
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-10-19 11:56:44 +0100

    Linting JS (http://jshint.com)

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,9 +1,8 @@
-// JavaScript Document
+/*global Cufon*/
 
-	//Cufon
-	Cufon.replace('.topbar .brand', { fontFamily: 'HouschkaAlt Heavy', textShadow: '#004369 1px 1px' }); 
-	Cufon.replace('.topbar a', { fontFamily: 'nevisBold' }); 
-	Cufon.replace('h2, h3, h4, h5, h6', { fontFamily: 'HouschkaAltBlackRegular'}); 
-	Cufon.replace('h1', { fontFamily: 'HouschkaAltBlackRegular', textShadow: '#004369 1px 1px' }); 
-	Cufon.replace('.page-header h2', { fontFamily: 'VAGLight' }); 
-	
+//Cufon
+Cufon.replace('.topbar .brand', { fontFamily: 'HouschkaAlt Heavy', textShadow: '#004369 1px 1px' });
+Cufon.replace('.topbar a', { fontFamily: 'nevisBold' });
+Cufon.replace('h2, h3, h4, h5, h6', { fontFamily: 'HouschkaAltBlackRegular'});
+Cufon.replace('h1', { fontFamily: 'HouschkaAltBlackRegular', textShadow: '#004369 1px 1px' });
+Cufon.replace('.page-header h2', { fontFamily: 'VAGLight' });
