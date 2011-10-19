0d41066782ab129479f06a2b25e58fd277559042
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-10-19 17:13:48 +0100

    404

---
+++ application/classes/controller/errors.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Errors extends Controller_Page {
+
+	public function action_404() {
+		//$this->auto_render = false;
+		$this->response->status(404);
+		$this->template->title = 'Page not found';
+		$this->template->subtitle = 'Error 404';
+		$this->template->content = View::factory('errors/404');
+	}
+
+}
