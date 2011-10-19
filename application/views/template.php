f1761e748d4e5e3b1dfb4cedc0caf7b82ac2bc10
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-10-19 14:26:47 +0100

    Google Analytics

--- application/views/template.php
+++ application/views/template.php
@@ -8,6 +8,9 @@
 <!--[if IE 7]>
 <link rel="stylesheet" href="/static/css/ie7styles.css" type="text/css" />
 <![endif]-->
+<script type="text/javascript">
+var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-26418668-1']);_gaq.push(['_trackPageview']);(function(){var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);})();
+</script>
 </head>
 <body>
 
