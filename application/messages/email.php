775884c3f16355d218676538c6a313a762e735d3
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-10-20 11:21:42 +0100

    Clean up emails

--- application/messages/email.php
+++ application/messages/email.php
@@ -4,31 +4,27 @@ return array(
 	// sent to an unregistered user
 	'invite' => array(
 		'subject' => '{friend_name} would like to be your friend on Gift Circle',
-		'plain'   => 'Hello {firstname} {surname}!
+		'plain'   => 'Hello {firstname} {surname},
 
 Register on GitftCircle: 
 <{register_link}>
-
-
 ',
 	),
 	
 	// sent to a registered user
 	'friend_request' => array(
 		'subject' => '{friend_name} would like to be your friend on Gift Circle',
-		'plain'   => 'Hello {firstname} {surname}!,
+		'plain'   => 'Hello {firstname} {surname},
 
 Log in to your account on GiftCircle: 
 <{login_link}>
-
-
 ',
 	),
 
 	// sent to a confirmed friend
 	'list_update' => array(
 		'subject' => '{friend_name}\'s list "{list_name}" has been updated on Gift Circle',
-		'plain'   => 'Hello {firstname} {surname}!,
+		'plain'   => 'Hello {firstname} {surname},
 
 {friend_name}\'s list "{list_name}" has been updated:
 
@@ -36,26 +32,30 @@ Log in to your account on GiftCircle:
 
 Log in to your account on GiftCircle: 
 <{login_link}>
-
-
 ',
 	),
 
 	// sent to someone who's reserved/bought a gift you've deleted
 	'deleted_gift' => array(
 		'subject' => '{owner_name} has just deleted a gift you\'ve reserved on Gift Circle',
-		'plain'   => 'Hello  {reserver_firstname} {reserver_surname},
+		'plain'   => 'Hello {reserver_firstname} {reserver_surname},
 
 Just to keep you in the loop, {owner_name} has just deleted the gift "{gift_name}" you\'ve reserved on GiftCircle.
+
+Log in to your account on GiftCircle: 
+<{url}>
 '
 	),
 
 	// sent to someone who's reserved/bought a gift you've edited
 	'edited_gift' => array(
 		'subject' => '{owner_name} has just edited a gift you\'ve reserved on Gift Circle',
-		'plain'   => 'Hello  {reserver_firstname} {reserver_surname},
+		'plain'   => 'Hello {reserver_firstname} {reserver_surname},
 
 Just to keep you in the loop, {owner_name} has just edited the gift "{gift_name}" you\'ve reserved on GiftCircle.
+
+Log in to your account on GiftCircle: 
+<{url}>
 '
 	),
 );
