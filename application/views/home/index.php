f3afd66293f0b45f041530a57982fa3d1ff28944
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-10-20 16:45:33 +0100

    Lorem

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -9,19 +9,19 @@
 		<div class="span8">
 			<ol>
 				<li>
-					<h4>A tool that saves you time</h4>
+					<h4>Lorem ipsum dolor sit amet</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 				</li>
 				<li>
-					<h4>Helps you budget</h4>
+					<h4>Lorem ipsum dolor sit amet</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 				</li>
 				<li>
-					<h4>Saves you money</h4>
+					<h4>Lorem ipsum dolor sit amet</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 				</li>
 				<li>
-					<h4>Everything is kept within your circle</h4>
+					<h4>Lorem ipsum dolor sit amet</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 				</li>
 			</ol>
@@ -29,19 +29,19 @@
 		<div class="span8">
 			<ol class="right">
 				<li>
-					<h4>Manage your children's gift lists</h4>
+					<h4>Lorem ipsum dolor sit amet</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 				</li>
 				<li>
-					<h4>Easy to understand and use</h4>
+					<h4>Lorem ipsum dolor sit amet</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 				</li>
 				<li>
-					<h4>Helps you get organised</h4>
+					<h4>Lorem ipsum dolor sit amet</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 				</li>
 				<li>
-					<h4>Reduces the stress of the big day</h4>
+					<h4>Lorem ipsum dolor sit amet</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 				</li>
 			</ol>
