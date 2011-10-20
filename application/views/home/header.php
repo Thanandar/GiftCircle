cdff6fec16ee5705ac8e40028876c42463dffd0a
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-10-20 16:46:19 +0100

    Homepage header view

---
+++ application/views/home/header.php
@@ -0,0 +1,32 @@
+
+
+<div class="row">
+
+	<div class="span7">
+
+		<p>
+			Welcome to Gift Circle.
+
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		</p>
+
+		<img src="http://placehold.it/400x140&text=Sketch" alt="Family" />
+
+		<button type="button" class="btn primary large" onclick="location.href='/user/register'">Get started in 30 seconds flat</button>
+
+	</div>
+
+	<div class="span9">
+		
+		<div style="  background: #fff; padding: 7px; height: 281px; margin: 10px 0 40px;
+
+  -webkit-box-shadow: 0px 0px 9px #666; 
+     -moz-box-shadow: 0px 0px 9px #666; 
+          box-shadow: 0px 0px 9px #666; 
+" >
+			<img src="http://placehold.it/504x280&text=Video" alt="Video" />
+		</div>
+
+	</div>
+
+</div>
