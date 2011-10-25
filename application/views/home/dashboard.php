fba185aa2afa46e6efcf62e063b6e748cd041b8b
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-10-25 11:08:21 +0100

    Hide 'All your circles' button when no circles exist

--- application/views/home/dashboard.php
+++ application/views/home/dashboard.php
@@ -15,8 +15,9 @@
 
 	<div class="well">
 		<input type="button" class="btn primary" value="New circle" onclick="location.href='/list/add'"/>
-
+		<?php if (count($my_lists)) { ?>
 		<a class="btn" href="/list/my">All your circles</a>
+		<?php } ?>
 	</div>
 
 	<h2>Your friends' circles</h2>
