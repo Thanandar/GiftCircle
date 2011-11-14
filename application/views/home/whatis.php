30917ca757f77dcb76b6c676bae489b7ab3e8a67
Tom <tom.quay@basecreativeagency.com>
2011-11-14 17:15:09 +0000

    updates to copy (h2 and what is GC?)

--- application/views/home/whatis.php
+++ application/views/home/whatis.php
@@ -4,17 +4,10 @@
 	<h2>Gift Circle...</h2>
 
 
-<p><img src="/img/chars/chars-circle.png" width="313" height="361" "Gift Circle" class="right" />Presents are great, but they can be a hassle. Frantic shopping, disappointed recipients, even receiving things that you really didn't want - All things that can bring down an event which is supposed to be a good time.</p>
-
-<p>If you never want to have to say "I still have the receipt if you don't like it...", or if you never want to have to nod and smile like you really wanted a matchstick model set, then Gift Circle is the gift buying tool you've been searching for. </p>
-
-<p>The idea is simple: Wishlists of wanted gifts you can share with whoever you like. Keep track of what others want and make sure they know what you want. Gift Circle makes it easy, with simple lists that you can add anything and anyone to. Then, your friends can reserve/buy without any of the awkward "research" chats or complicated web searching.</p>
-
-<p>Any occasion, any relationship, any gift. All you need to do is decide what you want or what you're going to buy.</p>
-
-<p>So why not get started? Sign up now and start creating lists, or start checking out what your friends are hoping to unwrap at the next big party.
-</p>
-
-
+<p><img src="/img/chars/chars-circle.png" width="313" height="361" "Gift Circle" class="right" />Presents are great, but can be a hassle. Frantic shopping, disappointed  recipients, even receiving things that you really didn't want - All things that  can bring down an event which is supposed to be a good time.</p>
+<p> If you never want to have  to say &quot;I still have the receipt if you don't like it...&quot;, or if you  never want to have to nod and smile like you really wanted a matchstick model  set for your 18th birthday, then Gift Circle is the gift buying tool  you've been searching for. </p>
+<p>The idea is simple: Get  your circle of close friends and family on giftcircle where you can all easily  build and swap lists of gifts you each want. Which means you’ll never be stuck  for a gift idea at the last minute again and you’ll probably get what you want.</p>
+<p>Loaded with easy to use  features like our Browser Button which with one click, copies details of a gift  you’d like from a website straight to your Gift List – great for putting a  wedding gift list together. </p>
+<p>So why not get started? <a href="/user/register/">Sign up now</a> and start creating lists, or start checking out what your friends and family are hoping to unwrap at the next big party.</p>
 </div>
 
