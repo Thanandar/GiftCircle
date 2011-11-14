34cd7c3f3e0f74f02fde626ed6bc54b6658dab49
Tom <tom.quay@basecreativeagency.com>
2011-11-14 17:16:14 +0000

    updates to copy (what is GC?)

--- application/views/home/whatis.php
+++ application/views/home/whatis.php
@@ -8,6 +8,6 @@
 <p> If you never want to have  to say &quot;I still have the receipt if you don't like it...&quot;, or if you  never want to have to nod and smile like you really wanted a matchstick model  set for your 18th birthday, then Gift Circle is the gift buying tool  you've been searching for. </p>
 <p>The idea is simple: Get  your circle of close friends and family on giftcircle where you can all easily  build and swap lists of gifts you each want. Which means you’ll never be stuck  for a gift idea at the last minute again and you’ll probably get what you want.</p>
 <p>Loaded with easy to use  features like our Browser Button which with one click, copies details of a gift  you’d like from a website straight to your Gift List – great for putting a  wedding gift list together. </p>
-<p>So why not get started? <a href="/user/register/">Sign up now</a> and start creating lists, or start checking out what your friends and family are hoping to unwrap at the next big party.</p>
+<p>So why not get started? <a href="/user/register/">Sign up now</a> and start creating lists, or start checking out what your friends and family are hoping to unwrap on their special occasion.</p>
 </div>
 
