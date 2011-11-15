9aa3c5ab5742398961201d43d587ec077c68aef2
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-11-15 15:38:18 +0000

    Video!

--- application/views/home/header.php
+++ application/views/home/header.php
@@ -11,7 +11,18 @@
      -moz-box-shadow: 0px 0px 9px #666; 
           box-shadow: 0px 0px 9px #666; 
 " >
-			<img src="/img/hp-video-temp.png" alt="Introduction" />
+			<a id="video-link" href="http://player.vimeo.com/video/32096592?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1"><img src="/img/hp-video-temp.png" alt="Introduction" /></a>
+			<script>
+				$("#video-link").fancybox({
+					'width'				: 640,
+					'height'			: 360,
+			        'autoScale'     	: false,
+			        'transitionIn'		: 'none',
+					'transitionOut'		: 'none',
+					'type'				: 'iframe'
+				});
+			</script>
+
 		</div>
 
 	</div>
