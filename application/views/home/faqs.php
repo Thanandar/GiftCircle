80aff2126b431d2547a112c4a7b4f1ff299ef2b6
Tom <tom.quay@basecreativeagency.com>
2011-11-20 21:22:14 +0000

    updated FAQ copy

--- application/views/home/faqs.php
+++ application/views/home/faqs.php
@@ -1,72 +1,73 @@
 
 <div class="span16 bigger">
-
-
-<h3>Q. So... What?</h3>
-<p><b>A.</b> Let's face it, it sucks to get the wrong gift. Ditzy mums, useless partners, interesting grandparents, and even sometimes your closest friends - Sometimes when it comes to presents they manage to go astray. And you've probably made a few mistakes in your lifetime too, even if you thought that look on your dad's face was genuine happiness.
-Here at Gift Circle we're putting a stop to bad gifts. How? With simple, usable, fun wishlists that make everyone happy.</p>
-
-<h3>Q. OK, so when's a good time to use Gift Circle?</h3>
-<p><b>A.</b> Any time there's an upcoming occasion in which the exchange of gifts is expected!</p>
-
-<h3>Q. Doesn't this take the surprise out of presents?</h3>
-<p><b>A.</b> 
-
-In short, no. At no point can you see whether a member of your circle has bought a gift from your list. What you will receive on the big day will remain a mystery until you start unwrapping.
-</p>
-
-<h3>Q. Alright, I'm in. How can I let my friends and family know what I want?</h3>
-<p><b>A.</b> If you're signed up, all you need to do is go to Dashboard and hit "New List". Give it a name and if you want, an expiry date. Add your gifts and add some friends and you're off!
-If you're not signed up... What are you waiting for?</p>
-
-<h3>Q. How can I get the right thing for my friends/family?</h3>
-<p><b>A.</b> Head over to Dashboard and check out the section titled "Friends Lists". If a gift is in the list, it's up for grabs! Reserve it to make sure others know not to buy it, and then purchase it! Job done!</p>
-
-<h3>Q. Let's get technical. How do I manage my created list?</h3>
-<p><b>A.</b> Go to Dashboard and click on a list you've created. You can add or remove gifts, and add or remove friends from this list/circle right here! (More on this below)</p>
-
-<h3>Q. Circles? Friends? How can I manage my acquaintances?</h3>
-<p><b>A.</b> Here at Gift Circle you have a list of confirmed "Friends", as in, anyone who is willing to buy a gift for you or have a gift bought for them by you. You can check them out by going to Friends.
-However, to help categorise people and to refine your wishlists (Let's face it, nobody wants Jim from the Office seeing what you want from your Wife on your anniversary) we've introduced Circles, which are essentially lists of friends that are related to a wishlist. It's a simple concept you may be familiar with. If you want to categorise your friends, we recommend you break them down into meaningful groups that are likely to attend the same events. Perhaps separate family from your closest friends, and both of those again from your workplace. You can have as many or as few circles/wishlists as you want.</p>
-<p>Adding friends is as simple as heading to one of your lists and hitting Add Friend. If they're signed up already it'll add them, if not, they'll be invited. </p>
-<p>If you're already friends with someone you can tick their checkbox then hit Add Friends to add them to this list/circle.</p>
-<p>Controlling your Circles is done in the list management area too. When you're on a list, click the garbage/bin icon to remove that friend from the list!</p>
-<p>You have total control over who can see which lists, and if worst comes to worst, you can always delete someone from your friends list entirely by selecting them from your friends list, then hitting Delete.</p>
-
-<h3>Q. Forget wishlists and friends - How can I manage my own profile?</h3>
-<p>Simple! Click your own name at the top right of any page, then hit Edit My Profile. You can alter your email, password and your name.</p>
-
-<h3>Q. I hate receiving emails, can I stop getting emailed every time something happens to a list?</h3>
-<p><b>A.</b> Yes! Just navigate to the list through the dashboard and hit Unsubscribe. Alternatively, if you buy something from the list we'll assume you don't want any more emails from that list unless it's directly related to what you've bought for someone.</p>
-<p>Equally, if you are extremely interested in a list and other gifts on it, you can always re-subscribe to the emails from a list by returning to it after you've bought something and hitting that button.</p>
-
-<h3>Q. What happens when lists are edited?</h3>
-<p><b>A.</b> If you mess with your list after a friend has reserved or bought something, we drop them an email to let them know. Equally, if something you're getting is edited, we send you an email, even if you're unsubscribed. No nasty surprises on the big day.</p>
-
-<h3>Q. Can I edit the details of a list, such as its name and expiry date?</h3>
-<p><b>A.</b> Yep! Just head to a list then click "Edit List Details".</p>
-
-<h3>Q. I made a big mistake; I want to delete a list completely.</h3>
-<p><b>A.</b> Head to a list, click "Edit List Details" and then "Delete this list". Make sure you're sure though!</p>
-
-<h3>Q. What's this browser button business about?</h3> 
-<p><b>A.</b> Good question!</p>
-<p>When you go to add gifts to your list, you can optionally decide to add a button to your bookmarks toolbar in your browser by clicking "Get the Browser Button". This streamlines the gift adding process by
-letting you click the button when you're on any other site and automatically add what you're looking at to one of your lists.
-Using this method you can still browse your favourite online stores in your usual way while creating your lists. It's the efficient, smooth way of using Gift Circle and we think it's pretty awesome.</p>
-
-<h3>Q. I don't see any bookmark toolbar to add the button to! Help!</h3>
-<p><b>A.</b> Not to worry. Different browsers handle this in different ways, so here's a handy guide to the most popular browsers and how to make them show the Bookmarks (or Favorites) Toolbar.</p>
-
-<p><b>Firefox:</b> Right click on an open area of the Firefox menu near the top of your screen, then check  Bookmarks Toolbar . Alternatively, Press Alt, then V, then T, then B. </p>
-<p><b>Chrome:</b> Click on the wrench icon in the top right, go to  Bookmarks , then check  Show Bookmarks Bar . Alternatively, press Ctrl + Shift + B (Windows) or Command + B (Mac) at the same time. </p>
-<p><b>Safari:</b> Click on the gear icon in the top right, and tick  Show Bookmarks Toolbar . Alternatively, Press Command + B (Mac) or Ctrl + Shift + B (Windows) at the same time. </p>
-<p><b>IE9:</b> Right click on an open area of the Internet Explorer menu near the top of your screen, then check  Favourites Bar . Alternatively, Press Alt, click View, then Toolbars. Now click Favorites bar </p>
-<p><b>IE7 or IE8:</b> Click Tools, then Toolbars. Now click Favourites Bar. </p>
-
-
-
-
+  <h3>Core  Concept</h3>
+  <p><strong>What is Gift Circle?</strong><br />
+    Gift Circle is an online tool that allows you  to easily build and swap gift lists with friends and family.  It’s a great way to save time and money whilst  ensuring you get the right gift every time. </p>
+  <p><strong>When do I use it?</strong><strong> </strong><br />
+    Add to your list  whenever you have a new gift idea and check friends and families lists whenever  there's an upcoming occasion.</p>
+  <p><strong>How do I get started?</strong><strong> </strong><br />
+    The best  way to get started is to view our 30 second intro animation which explains how  gift circles work. Don't have time for a 30 second animation? <a href="/list/add">Just jump in by  creating a new list</a> (you need to be logged in).</p>
+  <p><strong>Alright, I’m in how can I let my friends and family know what I want?</strong><br />
+    If you're signed up, all you need to do  is go to Dashboard and hit &quot;New List&quot;. Give it a name and if you  want, an expiry date. Add your gifts and add some friends and you're off! If  you're not signed up... <a href="/user/register">What are you waiting for</a>? </p>
+  <p><strong>Does it cost me anything?</strong><br />
+    No! Gift  Circle is a free resource. Create an account for life and add to it whenever  you want!</p>
+  <p>&nbsp;</p>
+  <h3>Gift  Lists</h3>
+  <p><strong>How do I view someone’s gift list?</strong><br />
+    To view a  gift list you need to be friends with the gift list creator. Then just click on  the user in <a href="/friend/list">your 'Friends'  page</a> (you need to be logged in) and you will be shown their most  recent gift lists. Click the relevant gift list and start browsing for a gift. </p>
+  <p><strong>How do share my list with friends and family?</strong><br />
+    Once you  have created a list you will be given the opportunity to invite your friends to  view it. All you need to do is add their e-mail address and they will get a  link to your gift list.</p>
+  <p><strong>I’ve got Wish lists on other sites such as  Amazon. Can I import these into GiftCircle?</strong><br />
+    Not at the  moment but we’re looking into it!</p>
+  <p><strong>What happens when I reserve an item on someone’s  gift list? </strong><br />
+    Once you’ve  reserved an item on someone’s gift list that item is no longer available for  other members of the list to reserve. You can reserve as many gifts as you like  but make sure that you remove them if you don’t end up buying them.</p>
+  <p><strong>What happens when lists are edited?</strong><br />
+    If you mess with  your list after a friend has reserved or bought something, we drop them an  email to let them know. Equally, if something you're getting is edited, we send  you an email, even if you're unsubscribed. No nasty surprises on the big day.</p>
+  <p><strong>Can I edit the  details of a list such as its name and expiry date</strong><br />
+    Yep! Just head to  a list then click &quot;Edit List Details&quot;.</p>
+  <p><strong>I made a big mistake; I want to delete a list  completely </strong><br />
+    Head to a list,  click &quot;Edit List Details&quot; and then &quot;Delete this list&quot;. Make  sure you're sure though!</p>
+  <p>&nbsp;</p>
+  <h3>Friends</h3>
+  <p><strong>How can I add/Invite someone to see my list?</strong><br />
+    Choose the  gift list you want that friend to be able to view and click the add friends  button. If you have not yet created a list, <a href="/list/add">get started here</a> (you need to be logged in).</p>
+  <p><strong>I’ve invited someone to my list but I can’t see  their lists.</strong><br />
+    In order to  be able to see a friends list they have to invite you to the list.</p>
+  <p><strong>I invited someone to my list but they didn’t  get the invite.</strong> <br />
+    We do all  we can to see that emails get to the intended recipient but it is not always  possible. Check the details you entered were correct and make sure you have the  recipients current e-mail address.</p>
+  <p><strong>Circle? Friends? How  can I manage my acquaintances?</strong><br />
+    Here at Gift Circle you have a list of  confirmed &quot;Friends&quot;, as in, anyone who is willing to buy a gift for  you or have a gift bought for them by you. You can check them out by going to  Friends. However, to help categorise people and to refine  your gift lists (Let's face it, nobody wants Jim from the Office seeing what  you want from your Wife on your anniversary) you can invite specific people to  specific lists. So you can be as public or as private with each list as suits  the event.</p>
+  <p>&nbsp;</p>
+  <h3>Shopping  List</h3>
+  <p><strong>If I reserve or click the’ item bought’ button,  will the user be notified?</strong><br />
+    Reserving  items is done behind the scenes so that a user can't see who's buying what;  just like a wedding register, no more double present embarrassments. Likewise  the bought item button is there to help you organize your shopping list and the  recipient will not be notified. </p>
+  <p><strong>Once I've chosen the gift where do I buy it?</strong><br />
+    Gift Circle  is more of a shopping assistant than a shop. We don't sell items directly but  we do know where’s best to buy. We use the information supplied by the gift  list creator to help you find the perfect gift. Once you've found a gift that  you want to get, click the browse button to get our suggested retailers.</p>
+  <p><strong>I still like going to the shops at Christmas can  I still use giftcircle without doing all my shopping online?</strong><br />
+    Of course!  The shopping list is designed to be printer friendly, reserve your presents  using Gift Circle and print out your Shopping Basket to make your trip to town  stress free!</p>
+  <p><strong>When is the last date I can shop online?</strong><br />
+    This year’s  online Christmas shopping cutoff is 18/12/11.</p>
+  <p>&nbsp; </p>
+  <h3>General</h3>
+  <p><strong>Something’s not working, what do I do?</strong><br />
+    If  something isn't working how you would expect it to work please get in contact  with us at <a href="mailto:help@giftcircle.co.uk">help@giftcircle.co.uk</a> and we will set it right as quickly as  possible.</p>
+  <p><strong>I want to advertise / be affiliated with you!</strong><br />
+    Great,  contact us at <a href="mailto:partners@giftcircle.co.uk">partners@giftcircle.co.uk</a></p>
+  <p><strong>How do I manage my  profile?</strong><br />
+    Simple! Click your own name at the top  right of any page, then hit Edit My Profile.</p>
+  <p><strong>Can I stop Gift Circle from E-mailing me?</strong><br />
+    Yes! Just navigate to the list through  the dashboard and hit Unsubscribe. Alternatively, if you buy something from the  list we'll assume you don't want any more emails from that list unless it's  directly related to what you've bought for someone.</p>
+  <p>Equally, if you are extremely interested  in a list and other gifts on it, you can always re-subscribe to the emails from  a list by returning to it after you've bought something and hitting that  button.</p>
+  <p><strong>What’s this bookmarklet or browser button  business about?</strong><br />
+    Good question!</p>
+  <p>When you go to add  gifts to your list, you can optionally decide to add a button to your bookmarks  toolbar in your browser by clicking &quot;Get the Browser Button&quot;. This  streamlines the gift adding process by letting you click the button when you're  on any other site and automatically add what you're looking at to one of your  lists. Using this method you can still browse your favorite online stores in  your usual way while creating your lists. It's the efficient, smooth way of  using Gift Circle and we think it's pretty awesome. <a href="/gift/bookmarklet/">Check it out here</a> (you need to be logged in).</p>
+  <p><strong>I don’t see any bookmark toolbar to add the  button to help!</strong><br />
+    Not to worry.  Different browsers handle this in different ways, so here's a handy guide to  the most popular browsers and how to make them show the Bookmarks (or  Favorites) Toolbar.</p>
+  <ul>
+    <li><strong>Firefox</strong>: Right  click on an open area of the Firefox menu near the top of your screen, then  check Bookmarks Toolbar . Alternatively, Press Alt, then V, then T, then B.</li>
+    <li><strong>Chrome</strong>: Click on  the wrench icon in the top right, go to Bookmarks , then check Show Bookmarks  Bar . Alternatively, press Ctrl + Shift + B (Windows) or Command + B (Mac) at  the same time.</li>
+    <li><strong>Safari</strong>: Click on  the gear icon in the top right, and tick Show Bookmarks Toolbar . Alternatively,  Press Command + B (Mac) or Ctrl + Shift + B (Windows) at the same time.</li>
+    <li><strong>IE9</strong>: Right click  on an open area of the Internet Explorer menu near the top of your screen, then  check Favourites Bar . Alternatively, Press Alt, click View, then Toolbars. Now  click Favorites bar</li>
+    <li><strong>IE7 or IE8</strong>: Click  Tools, then Toolbars. Now click Favourites Bar.</li>
+  </ul>
 </div>
-
-
