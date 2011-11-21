df5df96a938b26afa8cedc0885f5d3d2d385eb25
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-11-21 10:05:15 +0000

    Remove JavaScript confirm for ignore friend request

---
+++ application/views/friend/request_cancel.php
@@ -0,0 +1,19 @@
+
+<div class="span16">
+
+<p>
+	Are you sure you want to ignore this friend request?
+</p>
+
+<form action="" method="post">
+
+	<div class="well">
+		<input type="submit" class="btn primary" value="Ignore friend request" name="confirm" />
+		or
+		<a href="/">cancel</a>
+	</div>
+
+</form>
+
+</div>
+
