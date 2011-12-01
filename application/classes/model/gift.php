f21d22a83d0467bc8f6c50c9dd8912cf134d172e
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-12-01 16:46:21 +0000

    Don't try to email to deleted accounts

--- application/classes/model/gift.php
+++ application/classes/model/gift.php
@@ -127,16 +127,18 @@ class Model_Gift extends ORM {
 				$config->get('email-from-name')
 			);
 
-			if (is_object(Kohana::$log)) {
-				Kohana::$log->add(Log::INFO,
-					'Sending email To "' . implode(' ', $to) . '", '
-					. 'From "' . implode(' ', $from) . '", '
-					. 'Subject "' . $subject . '".'
-				);
-				Kohana::$log->write();
-			}
+			if ($reserver->email) {
+				if (is_object(Kohana::$log)) {
+					Kohana::$log->add(Log::INFO,
+						'Sending email To "' . implode(' ', $to) . '", '
+						. 'From "' . implode(' ', $from) . '", '
+						. 'Subject "' . $subject . '".'
+					);
+					Kohana::$log->write();
+				}
 
-			Email::send($to, $from, $subject, $message);
+				Email::send($to, $from, $subject, $message);
+			}
 
 		}
 
